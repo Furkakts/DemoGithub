9c2890c6cf388dd5c9233d6ee09ab1d119bfad54
Furkan Aktaş <aktasfurkan88@gmail.com>
2024-05-06 01:28:35 +0300

    Add HomeView

---
+++ Github/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Github
+//
+//  Created by Furkan on 6.05.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("fURKAN AKTAŞ IN sECOND 2")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
