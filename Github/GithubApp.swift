b8074fd0585bd56aeacae36cf0e015391a3369bd
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-05-05 14:15:30 +0300

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Furkan on 5.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
