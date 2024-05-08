281d4b4a174fa0cc68b3335884c46151ff3aa6fa
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-05-08 23:00:23 +0300

    [Feature] Subscribe 2 Added.

--- Github/ContentView.swift
+++ Github/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             Text("Clicked and Stashed!")
             Text("UnStage")
             Text("Subscribe Now")
+            Text("Subscribe 2")
             
         }
         .padding()
