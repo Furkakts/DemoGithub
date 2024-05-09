7b7950126441785c70f52ead3646932b30d59f93
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-05-09 21:31:46 +0300

    [Feature] Deleted abundant codes.

--- Github/ContentView.swift
+++ Github/ContentView.swift
@@ -10,25 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Stage")
-            Image(systemName: "heart.fill")
-                .font(.largeTitle)
-                .foregroundColor(.accentColor)
-            Text("Furkan!")
+            
             Button("Click Me!"){}
             Text("Clicked and Stashed!")
-            Text("UnStage")
+          
             VStack{
-                Text("Subscribe Now")
+               
                 Text("Subscribe 2")
                 Text("Subscribe 3")
                 Text("Subscribe 4")
-                Text("Subscribe 5")
-                Text("Subscribe 6")
-                Text("Subscribe 7")
-                Text("Subscribe 8")
-                Text("Subscribe 9")
+               
             }
+            
             
         }
         .padding()
