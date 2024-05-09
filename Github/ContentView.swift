a39de483fbb243d6dfc00a056233e760f12d5de4
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-05-09 22:26:14 +0300

    [Feature] Added New.

--- Github/ContentView.swift
+++ Github/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Subscribe 4")
+            Text("Subscribe 5")
+            Text("Subscribe 6")
         }
         .padding()
     }
